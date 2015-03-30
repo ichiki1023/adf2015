e6b398ca17a7e44411a23c4a194911592ad774e1
amutake <amutake.s@gmail.com>
2015-03-30 04:10:23 +0000

    misc

--- server.py
+++ server.py
@@ -3,21 +3,47 @@ from bottle import request, route, run
 
 import serial
 import time
-com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
+
+userdict = {}
+
+def newCom():
+  return
 
 def walk():
-  #コマンドの組み合わせ
-  # 前進
+  com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
   com.write("#M1")
-  time.sleep(5)
-  # 停止
+
+def stop():
+  com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
+  com.write("#M0")
+
+def walkstop():
+  com = serial.Serial('/dev/ttyAMA0', 57600) # , timeout = 1)
+  com.write("#M1")
+  time.sleep(1)
   com.write("#M0")
 
 @route('/')
 def getYo():
   username = request.query['username']
   print username
-  walk()
+  
+  if userdict.get(username) == None:
+    userdict[username] = 1
+  else:
+    userdict[username] = userdict[username] + 1
+
+  print username + ': ' + str(userdict[username])
+  # print len(userdict)
+  # print 'total: ' + str(
+
+  walkstop()
+  # if username == "AMUTAKE":
+  #   print "walk"
+  #   walkstop()
+  # else:
+  #   print "stop"
+  #   stop()
   return ""
 
 run(host='localhost', port=8080, debug=True)

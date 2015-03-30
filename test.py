2a63193f6baa0b4f34c6da4abe236a9dc8689cb0
satoichiki <i.tennis0210@gmail.com>
2015-03-30 13:17:30 +0900

    Update test.py
    yoユーザ全員にyoを返す関数です

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import sys
+# coding:utf-8
+import cgi,urllib
 
-print sys.stdin.encoding
+def yoAll(apitokenvalue):
+  	reqdata = {}
+	reqdata['api_token'] = apitokenvalue#YoのAPI_token
+	params = urllib.urlencode(reqdata)
+	urllib.urlopen("http://api.justyo.co/yoall/",params)
+
+yoAll("923474be-cde0-4ab1-a84f-171b4e78e821")

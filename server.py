b1e1ea0c783fb177fb9ff44d8eb65c1115ab369f
amutake <amutake.s@gmail.com>
2015-03-30 12:11:12 +0900

    walk

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
+# -*- coding: utf-8 -*-
 from bottle import request, route, run
+
+import serial
+import time
+com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
+
+def walk():
+  #コマンドの組み合わせ
+  # 前進
+  com.write("#M1")
+  time.sleep(5)
+  # 停止
+  com.write("#M0")
 
 @route('/')
 def getYo():
   username = request.query['username']
   print username
+  walk()
   return ""
 
 run(host='localhost', port=8080, debug=True)

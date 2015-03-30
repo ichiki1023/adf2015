35df46987c6cb8d44d4528953dc120a835836a51
amutake <amutake.s@gmail.com>
2015-03-30 04:56:25 +0000

    update

--- server.py
+++ server.py
@@ -1,33 +1,58 @@
 # -*- coding: utf-8 -*-
 from bottle import request, route, run
 
+from collections import deque
 import serial
 import time
 
-userdict = {}
+import Queue
 
-def newCom():
-  return
+import threading
 
-def walk():
+class WalkThread(threading.Thread):
+
+  def __init__(self, q):
+    super(WalkThread, self).__init__()
+    self.queue = q
+
+  def run(self):
+    while True:
+      n = self.queue.qsize()
+      if n != 0:
+        print n
+        walkstop(n)
+        while not self.queue.empty():
+          self.queue.get()
+
+def walkstop(sec):
+  com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 1)
+  com.write("#M1")
+  time.sleep(sec)
+  com.write("#M0")
+
+def walk(sec):
   com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
   com.write("#M1")
+  time.sleep(sec)
 
 def stop():
   com = serial.Serial('/dev/ttyAMA0', 57600, timeout = 10)
   com.write("#M0")
 
-def walkstop():
-  com = serial.Serial('/dev/ttyAMA0', 57600) # , timeout = 1)
-  com.write("#M1")
-  time.sleep(1)
-  com.write("#M0")
+
+queue = Queue.Queue()
+thread = WalkThread(queue)
+thread.start()
+
+userdict = {}
+
+
 
 @route('/')
 def getYo():
   username = request.query['username']
   print username
-  
+
   if userdict.get(username) == None:
     userdict[username] = 1
   else:
@@ -36,8 +61,9 @@ def getYo():
   print username + ': ' + str(userdict[username])
   # print len(userdict)
   # print 'total: ' + str(
+  queue.put(username)
 
-  walkstop()
+  # walkstop()
   # if username == "AMUTAKE":
   #   print "walk"
   #   walkstop()
